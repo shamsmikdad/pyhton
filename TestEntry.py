e70e885adbdd95e9e10c867b89a5ca5ae1efdc68
shamsmikdad <shams.mikdad@gmail.com>
2021-10-19 21:39:58 -0500

    faild

--- TestEntry.py
+++ TestEntry.py
@@ -1,25 +1,39 @@
 import tkinter as tk
 
-root = tk.Tk()
+self = tk.Tk()
+self.title('this is calculater')
+self.geometry("320x200")
 
-margin = 0.23
-projectedSales = tk.IntVar()
-profit = tk.IntVar()
+self.opration = ('+', '-', '*','/')
+self.option_var = tk.StringVar(self)
+number1 = int(self.entry1.get())
+number2 = int(self.entry2.get())
 
-entry = tk.Entry(root, textvariable=projectedSales)
+self.entry1 = tk.Entry(self)
+self.entry1.pack()
+self.entry2 = tk.Entry(self)
+self.entry2.pack()
 
-entry.pack()
+# option_menu = tk.OptionMenu(
+#             self,
+#             self.option_var,
+#             self.opration[0],
+#             *self.opration,
+#             # command=self.option_changed
+#             )
+# option_menu.grid(column=1, row=0, sticky=tk.W, **paddings)
+def calcualte (num1,num2):
+    res = num1+num2
+    return res
 
-def profit_calculator():
-    profit.set( margin * projectedSales.get())
+def calculator():
+    output_label['text'] = f'You result is: {calcualte(number1,number2)}'
 
-labelProSales = tk.Label(root,text="labelProSales", textvariable=projectedSales)
-labelProSales.pack()
 
-labelProfit = tk.Label(root,text="labelProfit", textvariable=profit)
-labelProfit.pack()
-
-button_calc = tk.Button(root, text="Calculate", command=profit_calculator)
+button_calc = tk.Button(self,text="calc", command=calculator)
 button_calc.pack()
 
-root.mainloop()
+output_label = tk.Label(self,text="labelProSales", foreground='red')
+output_label.pack()
+
+self.mainloop()
