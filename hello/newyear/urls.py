a92e5bfb2f684e0f1e6ec862c72d7997eb42cea7
shamsmikdad <shams.mikdad@gmail.com>
2021-10-21 02:50:00 -0700

    django-html

--- hello/newyear/urls.py
+++ hello/newyear/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 
 urlpatterns= [
     path("", views.index, name=("index")),
-    path("<str:yourname>", views.gere, name=("gere")),
+    # path("<str:yourname>", views.gere, name=("gere")),
     path("<int:birthday>", views.age, name=("age")),
-
-    
+    path("mario",views.mario,name=("mario")),   
 ]
--- hello/newyear/views.py
+++ hello/newyear/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.utils.timezone import datetime
+from django.http import HttpResponse, request
+import datetime
 
 # Create your views here.
 
@@ -8,9 +8,18 @@ def index(request):
     return render(request,"hi/index.html")
 
 def age(recuest,birthday):
-    year = datetime.today().year
-    yourAge = year-birthday
-    return HttpResponse(yourAge)
+    # year = datetime.today().year
+    # yourAge = year-birthday
+    Now = datetime.datetime.now()
 
-def gere(recuest,yourname):
-    return HttpResponse (f"hello hi: {yourname}")
+    return render (request,"hi/index.html",{
+        now.
+    })
+
+# def gere(request,yourname):
+#     return render (request,"greet/index.html",{
+#          "name": yourname.capitalize()
+#     })
+
+def mario(request):
+    return render(request,"mario/index.html")
